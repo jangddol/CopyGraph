1d2b9b66ebf3f2be3ee82fb7170344f593a0da53
jangddol <j991005@korea.ac.kr>
2024-06-25 01:45:07 +0900

    add type hint to get_file.py

--- get_file.py
+++ get_file.py
@@ -2,8 +2,8 @@ import os
 from docx import Document
 from PyPDF2 import PdfReader
 
-def get_file_paths(folder_path):
-    file_paths = []
+def get_file_paths(folder_path) -> list[str]:
+    file_paths: list = []
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.docx') or file.endswith('.pdf'):
@@ -11,17 +11,17 @@ def get_file_paths(folder_path):
     return file_paths
 
 
-def get_author(file_path):
+def get_author(file_path) -> str:
     # file_path의 파일명에서 첫 underscore 이후 나오는 열개의 숫자를 가지고 학번을 인식,
     # 학번이 중간에 포함된 파일을 찾아 그 중 text 파일이 있으면 첫줄만 읽어옮.
         # text 파일의 name는 기존의 file_name에서 3번째 underscore 이후의 문자열을 지우고, 3번째 underscore도 지우고 뒤에 .txt를 붙이면 됨.
     # 첫줄의 형식은 다음과 같음 "Name: {Name} . ({student ID:10 digits})"
     # 이를 통해 제출자의 이름을 추출
     # 추출한 이름을 authors 리스트에 추가
-    file_name = os.path.basename(file_path)
-    student_id = file_name.split('_')[1][:10]
-    text_file_name = file_name.split('_')[0] + '_' + file_name.split('_')[1] + '_' + file_name.split('_')[2] + '_' + file_name.split('_')[3] + '.txt'
-    text_file_path = os.path.join(os.path.dirname(file_path), text_file_name)
+    file_name: str = os.path.basename(file_path)
+    student_id: str = file_name.split('_')[1][:10]
+    text_file_name: str = file_name.split('_')[0] + '_' + file_name.split('_')[1] + '_' + file_name.split('_')[2] + '_' + file_name.split('_')[3] + '.txt'
+    text_file_path: str = os.path.join(os.path.dirname(file_path), text_file_name)
     if os.path.exists(text_file_path):
         try:
             # UTF-8 인코딩 시도
@@ -31,7 +31,7 @@ def get_author(file_path):
             try:
                 # CP949 인코딩 시도
                 with open(text_file_path, 'r', encoding='cp949') as file:
-                    author = file.readline().split(':')[1].strip().split('.')[0]
+                    author: str = file.readline().split(':')[1].strip().split('.')[0]
             except UnicodeDecodeError:
                 # 둘 다 실패하면 예외 발생
                 raise Exception(f"Failed to decode file: {text_file_path}")
@@ -53,29 +53,14 @@ def get_file_text(file_path) -> str:
     return text
 
 
-# def get_file_texts(file_paths):# -> list:
-#     texts = []
-#     for file_path in file_paths:
-#         texts(get_file_text(file_path))
-#     return texts
-
-
-# def get_authors(file_paths):# -> list:
-#     authors = []
-#     for file_path in file_paths:
-#         author = get_author(file_path)
-#         authors.append(author)
-#     return authors
-
-
-def get_file_dict(folder_path):# -> dict:
-    file_dict = {}
+def get_file_dict(folder_path) -> dict[str, str]:
+    file_dict: dict[str, str] = {}
     for root, dirs, files in os.walk(folder_path):
         for file in files:
             if file.endswith('.docx') or file.endswith('.pdf'):
-                file_path = os.path.join(root, file)
-                author = get_author(file_path)
-                text = get_file_text(file_path)
+                file_path: str = os.path.join(root, file)
+                author: str = get_author(file_path)
+                text: str = get_file_text(file_path)
                 # author can have multiple files
                 # if then, join the texts with a space
                 if author in file_dict:
