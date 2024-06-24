b65d9a2e4d37ad37a589a1564669220c653d9c01
jangddol <j991005@korea.ac.kr>
2024-06-25 01:47:10 +0900

    Refactor main_levenshtein.py, adding type hints and improving code structure

--- main_levenshtein.py
+++ main_levenshtein.py
@@ -5,22 +5,22 @@ from get_file import get_file_dict
 from similarity import make_nonlinear_similarity, plot_2d_graph
 
 
-def nonlinear_edge_weight(x : float):
+def nonlinear_edge_weight(x : float) -> float:
     x = 1 - x
     return pow(x, 9)
 
 
-def levenshtein_distance(s1, s2):
-    return Levenshtein.distance(s1, s2)
+def levenshtein_distance(s1, s2) -> float:
+    return float(Levenshtein.distance(s1, s2))
     
     
 if __name__ == '__main__':
     folder_path = 'D:\\Coding\\CopyGraph\\files\\FinalTuring'  # Replace with the path to your specific folder
-    file_dict = get_file_dict(folder_path)
+    file_dict: dict[str, str] = get_file_dict(folder_path)
     authors = list(file_dict.keys())
     texts = list(file_dict.values())
 
-    similarity_matrix = make_nonlinear_similarity(texts, levenshtein_distance, nonlinear_edge_weight)
+    similarity_matrix: list[list[float]] = make_nonlinear_similarity(texts, levenshtein_distance, nonlinear_edge_weight)
 
     # 네트워크 그래프 시각화
     plot_2d_graph(similarity_matrix, authors)
